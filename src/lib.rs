2018171ed61dd5379204d1308d94a5b0959bb18c
Renan Florez <renan@cloudwalk.io>
2022-02-08 16:16:55 -0300

    removing yaml specs from the IsoSpecs

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,38 @@
-// Copyright 2017 Rohit Joshi <rohit.c.joshi@gmail.com>
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
 #![feature(test)]
 #![allow(non_camel_case_types)]
 extern crate bit_array;
 extern crate typenum;
-extern crate yaml_rust;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_yaml;
 #[macro_use]
 extern crate log;
 
 pub mod iso_field;
 pub mod iso_msg;
-pub mod yaml_specs;
+pub mod iso_specs;
+
+pub struct ParsedMessage<'a> {
+    label: String,
+    value: &'a [u8],
+}
+
+pub fn parse_file(payload: &[u8]) -> Vec<ParsedMessage> {
+    let handle = iso_specs::IsoSpecs::new();
+
+    let mut current_message_pointer: usize = 0;
+    let mut messages_vec: Vec<ParsedMessage> = vec![];
+
+    // +2 because of the index being 0 and we need to surpass it by 1
+    while payload.len() > (current_message_pointer + 2) {
+        let iso_msg = iso_msg::IsoMsg::new(&handle, &payload[current_message_pointer..]);
+        for field in iso_msg.present_fields() {
+            let mut parsed_message = vec![ParsedMessage {
+                label: field.iso_field_label.clone().unwrap(),
+                value: field.iso_field_value(&payload[current_message_pointer..]),
+            }];
+            messages_vec.append(&mut parsed_message);
+        }
+        current_message_pointer += iso_msg.length();
+    }
+    messages_vec
+}

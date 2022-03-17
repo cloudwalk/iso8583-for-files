0ab83b5b94a73bc614323d23ee6b18ff0a3d4834
Renan Florez <renan@cloudwalk.io>
2022-03-17 18:27:12 -0300

    adding file utils to remove blocking and rdw

--- src/lib.rs
+++ src/lib.rs
@@ -6,35 +6,40 @@ extern crate serde_derive;
 #[macro_use]
 extern crate log;
 
+pub mod file_utils;
 pub mod iso_field;
 pub mod iso_msg;
 pub mod iso_specs;
 
 #[derive(Debug, Clone, Serialize)]
-pub struct Message<'a> {
+pub struct Message {
     label: String,
-    value: &'a [u8],
+    value: Vec<u8>,
 }
 
 #[derive(Debug, Clone, Serialize)]
-pub struct Group<'a> {
-    messages: Vec<Message<'a>>,
+pub struct Group {
+    messages: Vec<Message>,
 }
 
-pub fn parse_file(payload: &[u8]) -> Result<Vec<Group>, String> {
+pub fn parse_file(payload: Vec<u8>) -> Result<Vec<Group>, String> {
+    //checks if file has rdw at head and blocks at tail
+
     let handle = iso_specs::IsoSpecs::new();
 
     let mut current_message_pointer: usize = 0;
     let mut message_groups: Vec<Group> = vec![];
 
+    let clean_payload = file_utils::deblock_and_remove_rdw_from(payload);
+
     // +2 because of the index being 0 and we need to surpass it by 1
-    while payload.len() > (current_message_pointer + 2) {
+    while clean_payload.len() > (current_message_pointer + 2) {
         let mut messages_vec: Vec<Message> = vec![];
-        let iso_msg = iso_msg::IsoMsg::new(&handle, &payload[current_message_pointer..]);
+        let iso_msg = iso_msg::IsoMsg::new(&handle, &clean_payload[current_message_pointer..]);
         for field in iso_msg.present_fields() {
             let parsed_message = Message {
                 label: field.iso_field_label.clone().unwrap(),
-                value: field.iso_field_value(&payload[current_message_pointer..]),
+                value: field.iso_field_value(&clean_payload[current_message_pointer..]),
             };
 
             let mut parsed_message_vec = vec![parsed_message];
@@ -55,22 +60,23 @@ pub fn parse_file(payload: &[u8]) -> Result<Vec<Group>, String> {
     Ok(message_groups)
 }
 
+// this is an additional security to avoid a stack level too deep or endless-loops
 fn check_for_repeated_messages(messages_vec: &Vec<Message>) -> bool {
     let max_repeated_messages_count = 4;
     let scan_messages_count = 4;
 
     let last_parsed_value = match messages_vec.last() {
-        Some(x) => x.value,
-        None => &[],
+        Some(x) => x.value.clone(),
+        None => vec![],
     };
 
     let repeated_messages_count = messages_vec
         .iter()
         .rev()
         .take(scan_messages_count)
-        .map(|x| x.value)
-        .filter(|v| *v == last_parsed_value)
+        .map(|x| &x.value)
+        .filter(|v| *v == &last_parsed_value)
         .count();
 
-    max_repeated_messages_count == repeated_messages_count
+    max_repeated_messages_count <= repeated_messages_count
 }
